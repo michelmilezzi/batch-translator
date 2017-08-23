c82c3a49b8382da1bc8628f61c2ba45f3c1de704
Michel Milezzi <michelmilezzi@gmail.com>
2017-08-23 18:57:57 -0300

    updated translation script

--- tests.rb
+++ tests.rb
@@ -5,33 +5,18 @@ conf_constants = { 'brahmin' => 'vaca louca'}
 puts conf_constants
 puts conf_constants.class
 
-#friend3 = Hash.new
-#friend3["name"] = "Sassy"
-#friend3["breed"] = "Himalayan cat"
+new_cons = {}
 
-new_cons = Hash.new
-
-conf_constants.each do | k, v|
+conf_constants.each do |k, v|
   new_cons[k] = v
-  if !/[[:upper:]]/.match(k) 
-    new_cons[k.capitalize] = v  
-  end 
-end 
-
-# constants = Regexp.new(
-#   conf_constants.keys.map do |x|
-#     #/[[:upper:]]/.match(x) ? Regexp.escape(x) : "[#{Regexp.escape(x)}|#{Regexp.escape(x.capitalize)}]"
-#     #   Regexp.escape('[b|B]rahmin')
-# '[b|B]rahmin'
-#   end.join('|')
-# )
+  new_cons[k.capitalize] = v unless /[[:upper:]]/ =~ k
+end
 
 constants = Regexp.new(
   new_cons.keys.join('|')
 )
 
 puts constants
-#gsub: If the second argument is a Hash, and the matched text is one of its keys, the corresponding value is the replacement string.
 
 puts '------------------'
 
--- translator.rb
+++ translator.rb
@@ -10,14 +10,16 @@ translator = Google::Cloud::Translate.new(
   key: conf[:google_translate_api_key]
 )
 
-constants = Regexp.new(
-  conf[:constants].keys.map do |x|
-      # /[[:upper:]]/.match(x) ? Regexp.escape(x) : "[#{Regexp.escape(x)}|#{Regexp.escape(x.capitalize)}]"
-    Regexp.escape(x)
-  end.join('|')
-)
+new_cons = {}
 
-puts constants
+conf[:constants].each do |k, v|
+  new_cons[k] = v
+  new_cons[k.capitalize] = v unless /[[:upper:]]/ =~ k
+end
+
+constants = Regexp.new(
+  new_cons.keys.join('|')
+)
 
 conf[:constants].update(conf[:constants]) do |_, v|
   "<span class=\"notranslate\">#{v}</span>"
@@ -37,17 +39,23 @@ files.each_with_index do |file, i|
   original_phrases = File.read(file, encoding: conf[:file_encoding]).scan(conf[:text_chunks_pattern])
 
   original_phrases.each do |phrase|
-    phrases << phrase[0].gsub(constants, conf[:constants])
+    phrases << phrase[0].gsub(constants, new_cons)
   end
 
-  raw_translations = translator.translate phrases, from: conf[:source_language], to: conf[:target_language]
+  raw_translations = []
 
-  raw_translations.each_with_index do |raw_translation_object, i|
+  puts phrases
+  returned_translation = translator.translate phrases, from: conf[:source_language], to: conf[:target_language]
+
+  raw_translations.concat returned_translation if returned_translation.is_a?(Array)
+  raw_translations << returned_translation unless returned_translation.is_a?(Array)
+
+  raw_translations.each_with_index do |raw_translation_object, j|
     raw_translation = raw_translation_object.text
     raw_translation.encode!(conf[:file_encoding])
     raw_translation.gsub!(%r{<span class="notranslate">|<\/span>}, '')
     puts "Traduzido:#{raw_translation}"
-    target_file_content.gsub!(original_phrases[i][0], raw_translation)
+    target_file_content.gsub!(original_phrases[j][0], raw_translation)
   end
 
   File.open(target_path, 'w') { |file| file.puts target_file_content }
@@ -55,5 +63,5 @@ files.each_with_index do |file, i|
   # raw_translation = translator.translate phrases, from: conf['source_language'], to: conf['target_language']
   # puts raw_translation.class
   puts "---------#{i+1} de #{files.size} -------"
-  # sleep 0.5
+  sleep 0.25
 end
